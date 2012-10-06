ead6546e1dc5d3dab63642b04f6a46d28615f7b0
Sébastien Leduc <sebastien@sleduc.fr>
2012-10-06 02:40:08 +0200

    correction of regexps (missing P) + unittest for module pendu

--- pendu/__init__.py
+++ pendu/__init__.py
@@ -1,9 +1,12 @@
 #! /usr/bin/env python
 #-*- coding: utf-8 -*-
-from pendu import Pendu
 import os
 import string
 from pipobot.lib.modules import SyncModule, answercmd
+from pipobot.lib.module_test import ModuleTest
+from pendu import Pendu
+from pendu_ascii import asc_res
+
 
 VALID_CAR = string.ascii_lowercase + "-'"
 
@@ -20,42 +23,102 @@ pendu played : affiche la liste des lettres déjà jouées"""
                             bot,
                             desc=desc,
                             command="pendu")
-        self.bot.pendu = Pendu("")
+        self.pendu = Pendu("")
 
-    @answercmd("init", "init (?<word>\w+)")
+    @answercmd("init", "init (?P<word>\w+)")
     def init(self, sender, word=""):
-        if self.bot.pendu.word != "":
+        if self.pendu.word != "":
             return u"Il y a déjà une partie en cours… tu peux la finir, ou au pire 'reset' si tppt"
 
         if word == "":
             word_list = os.path.join(os.path.dirname(__file__), "wordlist.cfg")
-            self.bot.pendu.word = self.bot.pendu.create_word(word_list)
-            return u"Et c'est parti pour un pendu ! On cherche un mot de %s caractères" % len(self.bot.pendu.word)
+            self.pendu.word = self.pendu.create_word(word_list)
+            return u"Et c'est parti pour un pendu ! On cherche un mot de %s caractères" % len(self.pendu.word)
         else:
             word = word.strip().lower()
             if all([letter in VALID_CAR for letter in word]):
-                self.bot.pendu.word = word
-                self.bot.say(u"Et c'est parti pour un pendu ! On cherche un mot de %s caractères" % len(self.bot.pendu.word))
+                self.pendu.word = word
+                self.bot.say(u"Et c'est parti pour un pendu ! On cherche un mot de %s caractères" % len(self.pendu.word))
             else:
                 return u"Le mot choisi n'est pas valide ! (caractères acceptés : %s)" % VALID_CAR
 
     @answercmd("reset")
     def reset(self, sender):
-        self.bot.pendu.word = ""
+        self.pendu.word = ""
         return u"Reset effectué, plus qu'à utiliser init pour lancer une nouvelle partie"
 
-    @answercmd("(try|guess) (?<letter>\w)")
+    @answercmd("(try|guess) (?P<letter>\w)")
     def guess(self, sender, letter):
-        if self.bot.pendu.word == "":
+        if self.pendu.word == "":
             return "Euh, il faudrait lancer une partie…"
 
         if letter in VALID_CAR:
-            return self.bot.pendu.propose(letter)
+            return self.pendu.propose(letter)
         else:
             return "Il faut proposer une lettre !"
 
     @answercmd("(played|histo)")
     def played(self, sender):
-        if self.bot.pendu.word == "":
-            return "Euh, il faudrait lancer une partie…"
-        return self.bot.pendu.playedtostr()
+        if self.pendu.word == "":
+            return u"Euh, il faudrait lancer une partie…"
+        return self.pendu.playedtostr()
+
+
+class PenduTest(ModuleTest):
+    def test_pendu_success(self):
+        """Pendu scenario where the user finds the answer !"""
+        self.assertEqual(self.bot_answer("!pendu reset"),
+                         u"Reset effectué, plus qu'à utiliser init pour lancer une nouvelle partie")
+        self.assertEqual(self.bot_answer("!pendu played"),
+                         u"Euh, il faudrait lancer une partie…")
+
+        bot_rep = self.bot_answer("!pendu init pipoteam")
+        ok_tests = [("p", "p_p_____"), ("i", "pip_____"), ("e", "pip__e__")]
+        for letter, state in ok_tests:
+            bot_rep = self.bot_answer("!pendu try %s" % letter)
+            self.assertEqual(bot_rep, u"Bien vu ! Mot actuel: %s" % state)
+
+        self.assertEqual(self.bot_answer("!pendu try p"),
+                         u"Lettre déjà proposée")
+
+        expected = u"%s\nMot actuel : %s" % (asc_res[0], "pip__e__")
+        self.assertEqual(self.bot_answer("!pendu try z"),
+                         expected)
+
+        self.assertEqual(self.bot_answer("!pendu histo"),
+                         u"Lettres jouées: e, i, p, z")
+
+        ok_tests = [("o", "pipo_e__"), ("t", "pipote__"), ("a", "pipotea_")]
+        for letter, state in ok_tests:
+            bot_rep = self.bot_answer("!pendu try %s" % letter)
+            self.assertEqual(bot_rep, u"Bien vu ! Mot actuel: %s" % state)
+
+        bot_rep = self.bot_answer("!pendu try m")
+        self.assertEqual(bot_rep, u"Bien vu !\nEt oui, le mot à trouver était bien pipoteam !")
+
+    def try_8_wrong(self):
+        self.assertEqual(self.bot_answer("!pendu reset"),
+                         u"Reset effectué, plus qu'à utiliser init pour lancer une nouvelle partie")
+        bot_rep = self.bot_answer("!pendu init pipoteam")
+        for (index, letter) in enumerate("zryuqsdf"):
+            expected = u"%s\nMot actuel : %s" % (asc_res[index], "________")
+            self.assertEqual(self.bot_answer("!pendu try %s" % letter),
+                             expected)
+
+    def test_pendu_fail(self):
+        """Pendu scenario where the user does not find the answer !"""
+        self.try_8_wrong()
+        expected = "%sTu devais trouver pipoteam" % asc_res[8]
+        self.assertEqual(self.bot_answer("!pendu try k"),
+                         expected)
+
+    def test_last_minute_success(self):
+        """Finds the answer of the pendu at the last try !!"""
+        self.try_8_wrong()
+        ok_tests = [("p", "p_p_____"), ("i", "pip_____"), ("e", "pip__e__")]
+        ok_tests += [("o", "pipo_e__"), ("t", "pipote__"), ("a", "pipotea_")]
+        for letter, state in ok_tests:
+            bot_rep = self.bot_answer("!pendu try %s" % letter)
+            self.assertEqual(bot_rep, u"Bien vu ! Mot actuel: %s" % state)
+        bot_rep = self.bot_answer("!pendu try m")
+        self.assertEqual(bot_rep, u"Bien vu !\nEt oui, le mot à trouver était bien pipoteam !")
