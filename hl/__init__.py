f7a4a0d147f5b7c32fcd69daef746d71325fc68c
Sébastien Leduc <sebastien@sleduc.fr>
2012-10-19 11:10:32 +0200

    module urbandict

--- hl/__init__.py
+++ hl/__init__.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 #-*- coding: utf-8 -*-
 
-from model import HlList, HlListMembers
+from model import HlList
 from pipobot.lib.modules import SyncModule, defaultcmd, answercmd
 from pipobot.lib.known_users import KnownUser, minpermlvl
 
@@ -20,17 +20,16 @@ class HighLight(SyncModule):
                 desc=desc,
                 command='hl')
 
-    @answercmd(r'^show (?P<plist>\w+)')
+    @answercmd(r'show (?P<plist>\w+)')
     def aswer_show(self, sender, plist):
         hllist = self.bot.session.query(HlList).filter(HlList.name == plist).first()
         if not hllist:
             return _('I don\'t know that "%s" list' % plist)
         ret = _('list "%s"\'s members:' % plist)
-        for user in hllist.members:
-            ret += ' %s' % user.user
+        ret += ", ".join(map(str, hllist.members))
         return ret
 
-    @answercmd(r'^show')
+    @answercmd(r'show')
     def answer_showall(self, sender):
         hllists = self.bot.session.query(HlList).all()
         if not hllists:
@@ -38,8 +37,7 @@ class HighLight(SyncModule):
         ret = _("HighLight Lists:")
         for hllist in hllists:
             ret += "\n  %-31s" % hllist.name
-            for user in hllist.members:
-                ret += ' %s' % user.user
+            ret += ", ".join(map(str, hllist.members))
         return ret
 
     @answercmd(r'^set (?P<hllist>\w+) (?P<users>.*)')
@@ -49,99 +47,115 @@ class HighLight(SyncModule):
         unknownusers = []
         users = users.strip().split()
 
+        # We first check if users in the input users list are valid or not
         for user in users:
-            knownuser = KnownUser.get(user, self.bot)
-            if knownuser:
+            knownuser = KnownUser.get(user, self.bot, authviapseudo=True, in_live=False)
+            if knownuser is not None:
                 knownusers.append(knownuser)
             else:
                 unknownusers.append(user)
 
+        # If there are some invalid users
         if unknownusers:
-            ret += _("\nunknown users: %s" % unknownusers)
+            ret += _("\nunknown users: %s" % " ".join(unknownusers))
+
+        # If we have no valid user to add
         if not knownusers:
             ret += _("\n%s: You must provide at least one valid registered user" % sender)
             return ret.strip()
 
+        # Now we must add all knownusers to the hllist
+
+
         hllistentry = self.bot.session.query(HlList).filter(HlList.name == hllist).first()
+
+        # If the hllist does not exist, we create it
         if not hllistentry:
             hllistentry = HlList(hllist)
             self.bot.session.add(hllistentry)
+            ret += _('\nlist "%s" created' % hllist)
             self.bot.session.commit()
-            hllistentry = self.bot.session.query(HlList).filter(HlList.name == hllist).first()
-            ret += _('\nlist "%s" added' % hllist)
 
+        # Then we add all knownusers to the list
         for user in knownusers:
-            hllistmember = self.bot.session.query(HlListMembers).filter(HlListMembers.hlid == hllistentry.hlid).filter(HlListMembers.kuid == user.kuid).first()
-            if hllistmember:
+            if user in hllistentry.members:
                 ret += _('\n"%s" is already in list "%s"' % (user, hllistentry))
             else:
-                hllistmember = HlListMembers(hllistentry.hlid, user.kuid)
-                self.bot.session.add(hllistmember)
+                hllistentry.members.append(user)
+                self.bot.session.commit()
                 ret += _('\n"%s" added in list "%s"' % (user, hllistentry))
 
         self.bot.session.commit()
         return ret.strip()
 
-    @answercmd(r'^rm (?P<plist>\w+) (?P<users>.*)')
+    @answercmd(r'rm (?P<hllist>\w+)', r'rm (?P<hllist>\w+) (?P<users>.*)')
     @minpermlvl(2)
-    def answer_rm(self, sender, plist, users):
-        hllistname = plist
-        hllist = self.bot.session.query(HlList).filter(HlList.name == hllistname).first()
+    def answer_rm(self, sender, hllist, users=""):
+        ret = ''
+        # We search for the list <hllist>
+        hllist = self.bot.session.query(HlList).filter(HlList.name == hllist).first()
+
+        # If the list does not exist
         if not hllist:
             return _("%s: There is no such HighLight List" % sender)
-        ret = ''
 
         users = users.strip().split()
+        # If we want to remove users from the list
         if users:
             for user in users:
-                knownuser = KnownUser.get(user, self.bot)
+                # We search the knownuser associated to the username "user"
+                knownuser = KnownUser.get(user, self.bot, authviapseudo=True, in_live=True)
+                # If there is no result
                 if not knownuser:
                     ret += _('user "%s" is not even registered…\n' % user)
-                    continue
-                hllistmember = self.bot.session.query(HlListMembers).filter(HlListMembers.hlid == hllist.hlid).filter(HlListMembers.kuid == knownuser.kuid).first()
-                if not hllistmember:
+                # if the user is not in the list
+                elif not knownuser in hllist.members:
                     ret += _('user "%s" is not a member of this list\n' % knownuser)
                     continue
-                self.bot.session.delete(hllistmember)
-                ret += _('user "%s" has been deleted from list "%s"\n' % (knownuser, hllistname))
+                #if the user is in the list
+                else:
+                    hllist.members.delete(knownuser)
+                    ret += _('user "%s" has been deleted from list "%s"\n' % (knownuser, hllist))
+        # We remove the list
         else:
-            for member in hllist.members:
-                self.bot.session.delete(member)
-                ret += _('user "%s" has been deleted from list "%s"\n' % (member.user, hllistname))
-            self.bot.session.delete(hllist)
+            self.bot.session.delte(hllist)
             ret += _('list "%s" has been deleted' % hllistname)
+
         self.bot.session.commit()
         return ret.strip()
 
     @defaultcmd
     def answer(self, sender, message):
+        # !hl some people (: some messages)
         if not message:
             return self.desc
+
         knownusers = []
         unknownusers = []
-        hllists = self.bot.session.query(HlList).all()
         hllistnames = []
+        hllists = self.bot.session.query(HlList).all()
         for hllist in hllists:
             hllistnames.append(hllist.name)
         ret = 'HL:'
-        for user in message.split(':')[0].split(' '):
+        user_list, message = message.partition(":")[::2]
+        for user in user_list.split():
             if user in hllistnames:
                 for hllist in hllists:
                     if hllist.name == user:
-                        for knownuser in hllist.members:
-                            knownusers.append(knownuser.user)
+                        knownusers.extend(hllist.members)
                         break
             else:
-                knownuser = KnownUser.get(user, self.bot)
+                knownuser = KnownUser.get(user, self.bot, authviapseudo=True, in_live=True)
                 if knownuser:
                     knownusers.append(knownuser)
                 else:
                     unknownusers.append(user)
+
         for user in self.bot.occupants.users:
-            if KnownUser.get(user, self.bot) in knownusers:
-                ret += ' %s' % user
-        for user in unknownusers:
-            ret += ' %s' % user
-        if ':' in message:
-            ret += ' => ' + message.split(':', 1)[1].strip()
+            if KnownUser.get(user, self.bot, authviapseudo=True, in_live=True) in knownusers:
+                ret += ' %s ' % user
+        ret += " ".join(unknownusers)
+
+        if message.strip():
+            ret += u' → ' + message.strip()
         return ret
---
+++ urbandict/__init__.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+from collections import deque
+import json
+import urllib
+from pipobot.lib.modules import SyncModule, answercmd
+
+BASE_URL = "http://www.urbandictionary.com/iphone/search/define"
+MAX_CACHE = 10 
+
+
+class UrbanDict(SyncModule):
+    def __init__(self, bot):
+        desc = ("urban [result] [query] : displays the [result]-th response to the [query]\n"
+                "urban : displays a random urban dictionary entry")
+        SyncModule.__init__(self,
+                            bot,
+                            desc=desc,
+                            command="urban")
+        self._cache = deque()
+
+    def add_cache(self, request, result):
+        if len(self._cache) == MAX_CACHE:
+            self._cache.popleft()
+        self._cache.append((request, result))
+
+    def get_cache(self, request):
+        for req, result in self._cache:
+            if req == request:
+                return result
+
+    @answercmd("")
+    def random_urban(self, sender):
+        url = "http://www.urbandictionary.com/random.php"
+        page = urllib.urlopen(url)
+        content = page.read()
+        page.close()
+        # The random page is a redirection like this : 
+        # <html><body>You are being <a href="http://www.urbandictionary.com/define.php?term=RTM">redirected</a>.</body></html>
+        words = content.partition("define.php?term=")[2].partition('"')[0]
+        return self.urbandict(sender, req=words, select=0)
+
+
+    @answercmd("(?P<select>\d+)?(?P<req>.+)")
+    def urbandict(self, sender, req=None, select=None):
+        if req is None:
+            return self.random_urban(sender)
+        req = req.strip()
+        if select is None:
+            select = 0
+        else:
+            select = int(select)
+
+        defs = self.get_cache(req)
+        if defs is None:
+            params = urllib.urlencode({"term" : req})
+            page = urllib.urlopen("%s?%s" % (BASE_URL, params))
+            content = page.read()
+            page.close()
+            urban_json = json.loads(content)
+            if urban_json["result_type"] == "no_results":
+                self.add_cache(req, [])
+                return u"No result found"
+            defs = urban_json["list"]
+            self.add_cache(req, defs)
+        elif defs == []:
+            return u"Still no result found"
+
+        result = ""
+        if select >= len(defs):
+            result += "There are only %s definitions for this query : here is the first one : \n" % len(defs)
+            select = defs[0]
+        else:
+            select = defs[select]
+        result += u"%s : %s\nExample : %s" % (select["permalink"], select["definition"], select["example"])
+        return result
