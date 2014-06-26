f2760c86163a6202c059d41fc7af485895a12116
Guilhem Saurel <guilhem.saurel@gmail.com>
2014-06-26 16:26:41 +0900

    date_timezone: we do not flood/highlight everyone by default

--- date_timezone/__init__.py
+++ date_timezone/__init__.py
@@ -12,11 +12,16 @@ from .model import KnownUserTimeZone as KUTZ
 
 
 class CmdDateTimeZone(SyncModule):
-    _config = (("dateformat", str, '%a %d %b %Y, %X'), ("locale", str, 'fr_FR.UTF-8'),)
+    _config = (
+            ("server_timezone", str, "Europe/Paris"),
+            ("dateformat", str, '%a %d %b %Y, %X'),
+            ("locale", str, 'fr_FR.UTF-8'),
+            )
 
     def __init__(self, bot):
-        desc = _("date : show the actual date and time for all registered users\n")
+        desc = _("date : show the actual date for the server and the sender\n")
         desc += _("date set <timezone> : set your actual timezone (see http://pastebin.com/XbLSvZhY)\n")
+        desc += _("date all : show the actual date for all register users\n")
         desc += _("date <user> : show the actual date and time for <user>")
         SyncModule.__init__(self,
                             bot,
@@ -45,6 +50,17 @@ class CmdDateTimeZone(SyncModule):
         self.bot.session.commit()
         return _("Current timezone of %s set to %s" % (sender, tz))
 
+    @answercmd(r'^all')
+    def answer_all(self, sender):
+        session = self.bot.session
+        setlocale(LC_ALL, self.locale)
+        ret = _("It is:\n")
+        timezones = [tz[0] for tz in session.query(KUTZ.timezone).distinct().all()]
+        timezones_users = [(timezone(tz), ", ".join([k.user.pseudo for k in session.query(KUTZ).filter(KUTZ.timezone == tz).all()])) for tz in timezones]
+        timezones_users = sorted(timezones_users, key=lambda tz: datetime.now(tz[0]).timetuple())
+        ret += '\n'.join(['%s (%s): %s' % (datetime.now(tz).strftime(self.dateformat), tz.zone, users) for tz, users in timezones_users])
+        return ret
+
     @answercmd(r'^(?P<user>.+)')
     def answer_user(self, sender, user):
         knownuser = KnownUser.get(user, self.bot, authviapseudo=True)
@@ -54,15 +70,14 @@ class CmdDateTimeZone(SyncModule):
         if kutz is None:
             return _("I don't know %s's timezone, he has to '!date set <timezone>'" % user)
         setlocale(LC_ALL, self.locale)
-        return kutz.timezone + ': ' + datetime.now(timezone(kutz.timezone)).strftime('%A %d %B %Y, %X')
+        return kutz.timezone + ': ' + datetime.now(timezone(kutz.timezone)).strftime(self.dateformat)
 
     @defaultcmd
-    def answer(self, sender, message):
-        session = self.bot.session
-        setlocale(LC_ALL, self.locale)
-        ret = _("It is:\n")
-        timezones = [tz[0] for tz in session.query(KUTZ.timezone).distinct().all()]
-        timezones_users = [(timezone(tz), ", ".join([k.user.pseudo for k in session.query(KUTZ).filter(KUTZ.timezone == tz).all()])) for tz in timezones]
-        timezones_users = sorted(timezones_users, key=lambda tz: datetime.now(tz[0]).timetuple())
-        ret += '\n'.join(['%s (%s): %s' % (datetime.now(tz).strftime(self.dateformat), tz.zone, users) for tz, users in timezones_users])
+    def answer_default(self, sender, message):
+        ret = '%s (%s)' % (datetime.now(timezone(self.server_timezone)).strftime(self.dateformat), self.server_timezone)
+        knownuser = KnownUser.get(sender, self.bot)
+        if knownuser:
+            kutz = self.bot.session.query(KUTZ).filter(KUTZ.kuid == knownuser.kuid).first()
+            if kutz and kutz.timezone != self.server_timezone:
+                ret += '\n%s (%s)' % (datetime.now(timezone(kutz.timezone)).strftime(self.dateformat), kutz.timezone)
         return ret
