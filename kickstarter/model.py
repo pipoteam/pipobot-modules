07cdcd2a9d23d165a583b8c8cba35eba234047ea
Guilhem Saurel <guilhem.saurel@gmail.com>
2014-07-08 15:52:27 +0900

    New module: Kickstarter

---
+++ kickstarter/model.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+from sqlalchemy import Column, Integer, String
+import requests
+
+from pipobot.lib.bdd import Base
+
+
+class KickStart(Base):
+    __tablename__ = "kickstarter"
+    name = Column(String(250), primary_key=True)
+    owner = Column(String(250))
+
+    def url(self):
+        return 'https://www.kickstarter.com/projects/%s/%s/stats.json' % (self.owner, self.name)
+
+    def status(self):
+        ret = "%s: " % self.name
+        r = requests.get(self.url())
+        if r.status_code == 200:
+            json = r.json()['project']
+            return ret + _("%s, %s $ pledged") % (json['state'], json['pledged'])
+        return ret + _("HTTP status code %i") % r.status_code
