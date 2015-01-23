9f3912f48d608b4bb403f9e2835e63273a0885fc
Guilhem Saurel <guilhem.saurel@gmail.com>
2015-01-23 17:32:18 +0100

    9gag module. Sorry. blame @Adr921

---
+++ 9gag/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from pipobot.lib.modules import SyncModule, defaultcmd
+from pipobot.lib.utils import check_url
+
+class Gags(SyncModule):
+    def __init__(self, bot):
+        desc = _('retrieves a random 9gag page.')
+        SyncModule.__init__(self,
+                                 bot,
+                                 desc=desc,
+                                 name="9gag")
+
+    @defaultcmd
+    def answer(self, sender, message):
+        return check_url('http://9gag.com/random', geturl=True)
