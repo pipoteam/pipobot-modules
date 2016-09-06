7ae196c0afd69f5084ed5eafa4c2617f048b8e37
Guilhem Saurel <guilhem.saurel@gmail.com>
2016-09-06 14:44:16 +0200

    created time in last_words, fix #15

--- last_words/model.py
+++ last_words/model.py
@@ -1,8 +1,11 @@
+import datetime
+
 from pipobot.lib.bdd import Base
-from sqlalchemy import Column, ForeignKey, Integer, Text
+from sqlalchemy import Column, ForeignKey, Integer, Text, DateTime
 
 
 class LastWords(Base):
     __tablename__ = 'last_words'
     kuid = Column(Integer, ForeignKey('knownuser.kuid'), primary_key=True)
     message = Column(Text(1000))
+    created = Column(DateTime, onupdate=datetime.datetime.now)
