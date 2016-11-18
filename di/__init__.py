860f8366cce302ca6a1b66f97374de3cf07714c1
Guilhem Saurel <guilhem.saurel@gmail.com>
2016-11-18 16:15:45 +0100

    module di

---
+++ di/__init__.py
@@ -0,0 +1,13 @@
+from pipobot.lib.modules import ListenModule
+
+
+class DiAnswer(ListenModule):
+    def __init__(self, bot):
+        desc = "Pipo dit"
+        ListenModule.__init__(self, bot, name="dianswer", desc=desc)
+
+    def answer(self, sender, message):
+        message = message.lower()
+        for di in ["di", "dy", "d'y", "d’y"]:
+            if di in message:
+                return message.split(di, 1)[1]
