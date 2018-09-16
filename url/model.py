17af056f9a5b5eb4a1e7f2aeca61fe513cb51026
Simon THOBY <simonthoby@live.fr>
2018-09-16 13:32:12 +0200

    URL: Do not send "OLD!" messages whenever the link is quite fresh

--- url/model.py
+++ url/model.py
@@ -9,13 +9,17 @@ class RepostUrl(Base):
     __tablename__ = "url"
     url = Column(String(250), primary_key=True)
     count = Column(Integer)
+    title = Column(String(250))
     date = Column(DateTime)
+    last_date = Column(DateTime)
     jid = Column(String(250))
     chan = Column(String(250))
 
-    def __init__(self, url, jid, chan):
+    def __init__(self, url, jid, chan, title=""):
         self.url = url
         self.jid = jid
         self.count = 1
+        self.title = title
         self.date = datetime.datetime.now()
+        self.last_date = self.date
         self.chan = chan
